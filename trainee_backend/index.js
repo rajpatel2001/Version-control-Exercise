c469226dae28eb8c258b7ffaa731bfb8a354c4f9
rajpatel2001 <raj.d@simformsolutions.com>
2023-02-21 10:23:28 +0530

    backend commit

---
+++ trainee_backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get("/api", (req,res) => {
+    res.json({"users": ["Raj","Rajan","kishan","Nilay","Yash"]})
+})
+
+app.listen(5000, ()=> {console.log("server started on port 5000")})
