adb939ff500b2a18038c40905e94b287ea1fa9bc
rajpatel2001 <raj.d@simformsolutions.com>
2023-02-21 10:54:27 +0530

    add user

--- trainee_backend/index.js
+++ trainee_backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req,res) => {
-    res.json({"users": ["Raj","Rajan","kishan","Nilay","Yash"]})
+    res.json({"users": ["Raj","Rajan","kishan","Nilay","Yash","Kavya"]})
 })
 
 app.listen(5000, ()=> {console.log("server started on port 5000")})
